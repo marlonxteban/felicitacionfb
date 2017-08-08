2629c22e0062a9437e33e5b78f682ff274747379
marlonxteban <marlonxteban@gmail.com>
2017-08-08 16:22:08 -0500

    pruebas de timeline y json

--- felicitaciontw.py
+++ felicitaciontw.py
@@ -23,15 +23,29 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-
+#f = open('resp.txt', 'w')
 # If the authentication was successful, you should
 # see the name of the account print out
+#f.write(api.me().name)
 print(api.me().name)
-friends = api.me().friends
+
 #print(json.dumps(api.me()._json, sort_keys=True, indent=4))
 f = open('resp.txt', 'w')
-print(api.me().timeline(), file="resp.txt")
+feed=api.me().timeline(count=30)
+#print(api.me().timeline())
+#print(json.dumps(api.rate_limit_status(), indent=4, sort_keys=False))
+#print(len(feed))
+
+for tweet in feed:
+	#print(type(tweet._json))
+	print('\n')
+	#f.write(json.dumps(tweet._json, indent=4))
+	print(tweet._json['text'])
+f.close()
 # If the application settings are set for "Read and Write" then
 # this line should tweet out the message to your account's
 # timeline. The "Read and Write" setting is on https://dev.twitter.com/apps
 #api.update_status(status='Updating using OAuth authentication via Tweepy!')
+#for status in feed:
+#	f.write(str(status))
+#f.close()
